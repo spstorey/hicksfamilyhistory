4574253fae5d80019bf50d3bcb9ac3ef7cefd06f
Shaun Storey <shaun@spssoftware.co.uk>
2014-05-07 21:33:07 +0100

    Initial commit

---
+++ src/main/java/spssoftware/ApplicationWebXml.java
@@ -0,0 +1,12 @@
+package spssoftware;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ApplicationWebXml extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
